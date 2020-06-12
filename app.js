60b98fffbbb1477f0e671ce2b47d964332ba18cb
giovanilgg <giovanilopez82@aragon.unam.mx>
2020-06-11 21:31:31 -0500

    Se agrego un index

--- app.js
+++ app.js
@@ -16,7 +16,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
+app.get('/', (req, res) => {
+
+    res.render('index');
+
+
+})
 
 app.use(logger('dev'));
 app.use(express.json());
