267785fcb63cf7548e4c27b0b11d472bcf3fd9a2
crist-start <cristart.cs@gmail.com>
2020-06-16 18:25:29 -0500

    peticion patch terminada, vista frond-end de patch(update) en fase 1 de 2

--- routes/patch/patch.js
+++ routes/patch/patch.js
@@ -6,8 +6,9 @@ router.patch('/',function (req,res,next) {
   res.status(405).json({mensaje:"accion no permitida"});
 });
 router.patch('/:id',function (req,res,next) {
-  campo=req.params.campo
-  dino.findOneAndUpdate({nombre:req.params.id},{tam:req.body.dato},{new:true},function (err,dato) {
+  campo={};
+  campo[req.body.campo]=req.body.dato;
+  dino.findOneAndUpdate({id:req.params.id},campo,{new:true},function (err,dato) {
     if(dato==null){
       res.status(404).json({mensaje:"dato no encontrado"});
     }else{
