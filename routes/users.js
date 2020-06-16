a2d58fb44bb2ff35237d4568bb39c4beec7186f9
giovanilgg <giovanilopez82@aragon.unam.mx>
2020-06-16 14:29:21 -0500

    se agrego una peticion get para determinar un rango de registros

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,44 @@ var express = require('express');
 var router = express.Router();
 const Usuario = require('../collection/dinosaurios');
 
-
-/* GET users listing. */
 router.get('/dinos', function(req, res) {
 
+
     Usuario.find({})
+
+    .exec((err, dinosaurios) => {
+
+        if (err) {
+
+            return res.status(400).json({
+                ok: false,
+                err
+
+            })
+        }
+        res.json({
+            ok: true,
+            dinosaurios
+
+
+        });
+
+    })
+
+
+});
+router.get('/dinos/rango', function(req, res) {
+    let desde = req.query.desde || 0; //parametros opcionales entran en query
+    desde = Number(desde);
+    //si no recibe un parametro desde inicialo desde 0
+
+    let limite = req.query.limite || 10;
+    limite = Number(limite);
+    //si no existe un parametro limite te arroja 10 registros
+
+    Usuario.find({})
+        .skip(desde)
+        .limit(10) //establece un limite de 10 en 10 
         .exec((err, dinosaurios) => {
 
             if (err) {
@@ -28,6 +61,8 @@ router.get('/dinos', function(req, res) {
 
 
 });
+
+
 
 
 router.post('/dinos', (req, res) => {
@@ -72,26 +107,26 @@ router.post('/dinos', (req, res) => {
 });
 
 router.delete('/', (req, res) => {
-    res.status(405).json({message: 'No se permite'})
- });
- 
- router.delete('/dinos/:id', (req, res) => {
-     let id = req.params.id;
-     Usuario.findByIdAndRemove(id, (err, dinoBorrado) => {
-         if (!dinoBorrado) {
-             return res.status(400).json({
-                 ok: false,
-                 err: {
-                     message: 'el dinosaurio no se encontro encontrado'
-                 }
-             })
-         }
-         res.json({
-             ok: true,
-             dino: dinoBorrado
-         })
-     })
- })
- 
+    res.status(405).json({ message: 'No se permite' })
+});
+
+router.delete('/dinos/:id', (req, res) => {
+    let id = req.params.id;
+    Usuario.findByIdAndRemove(id, (err, dinoBorrado) => {
+        if (!dinoBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'el dinosaurio no se encontro encontrado'
+                }
+            })
+        }
+        res.json({
+            ok: true,
+            dino: dinoBorrado
+        })
+    })
+})
+
 
 module.exports = router;
