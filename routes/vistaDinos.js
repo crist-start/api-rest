bc48a2efb15fd2f165b40cedd6f3f36fba11489e
crist-start <cristart.cs@gmail.com>
2020-06-17 16:23:48 -0500

    cambio de enlaces para su funcionamiento en heroku

--- routes/vistaDinos.js
+++ routes/vistaDinos.js
@@ -3,7 +3,7 @@ var router = express.Router()
 const request = require('request');
 
 router.get('/dinosaurios', function(req, res, next) {
-    request.get('http://localhost:3000/users/dinos', function(err, response, body) {
+    request.get('https://apidinos.herokuapp.com/users/dinos', function(err, response, body) {
         if (err) response.status(400).json({ mensage: 'no se encontraron dinosaurios' });
         else res.render('vistaDino', JSON.parse(body))
     });
