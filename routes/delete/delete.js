583c2b351a06e80dbafa03d45a215f3347043176
giovanilgg <giovanilopez82@aragon.unam.mx>
2020-06-17 20:11:59 -0500

    cambio

--- routes/delete/delete.js
+++ routes/delete/delete.js
@@ -14,7 +14,7 @@ router.delete('/borrar/:id', async(req, res) => { //se tiene una peticion asincr
         if (err) status(400).json({ mensaje: 'no se encontro el dino' });
         else res.status(200).json({
             ok: 'dinosaurio eliminado',
-            dino
+            dinosaurio: dino
         });
 
     })
